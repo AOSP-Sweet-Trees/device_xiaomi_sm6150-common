815152d307451045c053e402c8fb05e6fe301f84
Adhitya Mohan <me@adhityamohan.in>
2021-11-24 14:55:18 +0100

    sm6150-common: parts: Implement HBM switch

--- parts/src/org/lineageos/settings/display/DisplaySettingsFragment.java
+++ parts/src/org/lineageos/settings/display/DisplaySettingsFragment.java
@@ -22,23 +22,26 @@ import android.view.MenuItem;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import org.lineageos.settings.R;
 import org.lineageos.settings.utils.FileUtils;
 
-public class DcDimmingSettingsFragment extends PreferenceFragment implements
+public class DisplaySettingsFragment extends PreferenceFragment implements
         OnPreferenceChangeListener {
 
-    private SwitchPreference mDcDimmingPreference;
+    private SwitchPreferenceCompat mDcDimmingPreference;
     private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
     private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_bl";
+    private SwitchPreferenceCompat mHBMPreference;
+    private static final String HBM_ENABLE_KEY = "hbm_mode";
+    private static final String HBM_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        addPreferencesFromResource(R.xml.dcdimming_settings);
+        addPreferencesFromResource(R.xml.display_settings);
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
-        mDcDimmingPreference = (SwitchPreference) findPreference(DC_DIMMING_ENABLE_KEY);
+        mDcDimmingPreference = (SwitchPreferenceCompat) findPreference(DC_DIMMING_ENABLE_KEY);
         if (FileUtils.fileExists(DC_DIMMING_NODE)) {
             mDcDimmingPreference.setEnabled(true);
             mDcDimmingPreference.setOnPreferenceChangeListener(this);
@@ -46,12 +49,23 @@ public class DcDimmingSettingsFragment extends PreferenceFragment implements
             mDcDimmingPreference.setSummary(R.string.dc_dimming_enable_summary_not_supported);
             mDcDimmingPreference.setEnabled(false);
         }
+        mHBMPreference = (SwitchPreferenceCompat) findPreference(HBM_ENABLE_KEY);
+        if (FileUtils.fileExists(HBM_NODE)) {
+            mHBMPreference.setEnabled(true);
+            mHBMPreference.setOnPreferenceChangeListener(this);
+        } else {
+            mHBMPreference.setSummary(R.string.hbm_enable_summary_not_supported);
+            mHBMPreference.setEnabled(false);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (DC_DIMMING_ENABLE_KEY.equals(preference.getKey())) {
             FileUtils.writeLine(DC_DIMMING_NODE, (Boolean) newValue ? "1":"0");
+        }
+        if (HBM_ENABLE_KEY.equals(preference.getKey())) {
+            FileUtils.writeLine(HBM_NODE, (Boolean) newValue ? "1" : "0");
         }
         return true;
     }
