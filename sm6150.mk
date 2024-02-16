a3dc1fe0fbecb900e5ff2a440222a32bc79ed5ad
aswin7469 <aswinas@pixysos.com>
2024-02-16 10:31:41 +0000

    sm6150-common: Initialized Spatial Audio Change-Id: I4fcbd8d8c94a1d1f706e45f38292b840be2b7c44 Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com> Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -347,6 +347,22 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/pixel \
     hardware/xiaomi
 
+# Spatial Audio
+PRODUCT_PACKAGES += \
+    libspatialaudio
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+       audio.spatializer.effect.util_clamp_min=300
+
+PRODUCT_PROPERTY_OVERRIDES += \
+       ro.audio.spatializer_enabled=true \
+       ro.audio.spatializer_transaural_enabled_default=false \
+       persist.vendor.audio.spatializer.speaker_enabled=true \
+       ro.vendor.audio.feature.spatial=7
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
