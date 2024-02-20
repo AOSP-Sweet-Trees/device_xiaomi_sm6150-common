9d66cfeab9bd77a0896a9720e385436c64b4ddc0
ugly-kid-af <dwarmachine24@gmail.com>
2024-02-20 09:01:58 +0530

    sm6150-common: re-order RemovePackages
    h

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,20 +4,46 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics
-LOCAL_OVERRIDES_PACKAGES += DCMO DevicePolicyPrebuilt DMService Drive 
-LOCAL_OVERRIDES_PACKAGES += GCS GoogleCamera
-LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices
-LOCAL_OVERRIDES_PACKAGES += PixelLiveWallpaperPrebuilt Photos PrebuiltGmail
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += Tycho
-LOCAL_OVERRIDES_PACKAGES += USCCDM
-LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger
-LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+          arcore \
+          AmbientSensePrebuilt \
+          AppDirectedSMSService \
+          ConnMO \
+          CalendarGooglePrebuilt \
+          Drive \
+          DCMO \
+          DevicePolicyPrebuilt \
+          DMService \
+          GCS \
+          GoogleCamera \
+          MaestroPrebuilt \
+          Maps \
+          MicropaperPrebuilt \
+          MyVerizonServices \
+          OBDM_Permissions \
+          obdm_stub \
+          PixelWallpapers2021 \
+          PixelWallpapers2020 \
+          RecorderPrebuilt \
+          SCONE \
+          ScribePrebuilt \
+          Showcase \
+          Snap \
+          SoundAmplifierPrebuilt \
+          SprintDM \
+          SprintHM \
+          TurboPrebuilt \
+          Tycho \
+          TipsPrebuilt \
+          talkback \
+          USCCDM \
+          VZWAPNLib \
+          VzwOmaTrigger \
+          WallpapersBReel2020 \
+          YouTube \
+          YouTubeMusicPrebuilt
+
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
